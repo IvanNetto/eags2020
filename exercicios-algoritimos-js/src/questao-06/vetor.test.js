89d450b797471e79c950aa21f6e79e54bc161966
Diego Moura <diegosouza20049@hotmail.com>
2020-09-07 16:46:44 -0300

    correção de typos

--- exercicios-algoritimos-js/src/questao-06/vetor.test.js
+++ exercicios-algoritimos-js/src/questao-06/vetor.test.js
@@ -1,3 +1,4 @@
+/* eslint-env jest */
 const vetor1 = require('./vetor-1')
 const vetor2 = require('./vetor-2')
 const vetor3 = require('./vetor-3')
@@ -6,7 +7,7 @@ const vetor4 = require('./vetor-4')
 describe('QUESTÃO 6 - UTILIZANDO O VETOR INICIAL', () => {
     test('DADO O VETOR [32,45,89,66,12,35,10,96,38,15,13,11,65,81,35,64,16,89,54,19] DEVE RETORNAR O MENOR VALOR QUE SERÁ 11', () => {
         const given = {
-            vetor =[32, 45, 89, 66, 12, 35, 10, 96, 38, 15, 13, 11, 65, 81, 35, 64, 16, 89, 54, 19]
+            vetor: [32, 45, 89, 66, 12, 35, 10, 96, 38, 15, 13, 11, 65, 81, 35, 64, 16, 89, 54, 19]
         }
 
         const expected = 11
@@ -16,7 +17,7 @@ describe('QUESTÃO 6 - UTILIZANDO O VETOR INICIAL', () => {
 
     test('DADO O VETOR [32,45,89,66,12,35,10,96,38,15,13,11,65,81,35,64,16,89,54,19] DEVE RETORNAR O MAIOR VALOR QUE SERÁ 96', () => {
         const given = {
-            vetor =[32, 45, 89, 66, 12, 35, 10, 96, 38, 15, 13, 11, 65, 81, 35, 64, 16, 89, 54, 19]
+            vetor: [32, 45, 89, 66, 12, 35, 10, 96, 38, 15, 13, 11, 65, 81, 35, 64, 16, 89, 54, 19]
         }
 
         const expected = 96
@@ -27,7 +28,7 @@ describe('QUESTÃO 6 - UTILIZANDO O VETOR INICIAL', () => {
 
     test('DADO O VETOR [32,45,89,66,12,35,10,96,38,15,13,11,65,81,35,64,16,89,54,19] DEVE RETORNAR O VETOR DOS NUMEROS PARES [32,66,12,10,96,38,64,16,54]', () => {
         const given = {
-            vetor =[32, 45, 89, 66, 12, 35, 10, 96, 38, 15, 13, 11, 65, 81, 35, 64, 16, 89, 54, 19]
+            vetor: [32, 45, 89, 66, 12, 35, 10, 96, 38, 15, 13, 11, 65, 81, 35, 64, 16, 89, 54, 19]
         }
 
         const expected = [32, 66, 12, 10, 96, 38, 64, 16, 54]
@@ -38,7 +39,7 @@ describe('QUESTÃO 6 - UTILIZANDO O VETOR INICIAL', () => {
 
     test('DADO O VETOR [32,45,89,66,12,35,10,96,38,15,13,11,65,81,35,64,16,89,54,19] DEVE RETORNAR O VETOR DOS NUMEROS IMPARES [45, 89, 35, 15, 13, 11, 65, 81, 35, 89, 19]', () => {
         const given = {
-            vetor =[32, 45, 89, 66, 12, 35, 10, 96, 38, 15, 13, 11, 65, 81, 35, 64, 16, 89, 54, 19]
+            vetor: [32, 45, 89, 66, 12, 35, 10, 96, 38, 15, 13, 11, 65, 81, 35, 64, 16, 89, 54, 19]
         }
 
         const expected = [45, 89, 35, 15, 13, 11, 65, 81, 35, 89, 19]
